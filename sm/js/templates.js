10022eca69e33b00087f5c541ff8e5f008b69ad1
Seezar75 <cesare.ali@gmail.com>
2024-02-28 09:20:48 +0100

    Added Escher lizards tiling

--- sm/js/templates.js
+++ sm/js/templates.js
@@ -599,6 +599,92 @@ function setTestShape() {
 	console.log(JSON.stringify(templateRels));
 }
 
+function setEscher() {
+	templateRels.push([]);
+	templateRels.push([]);
+	templateRels.push([]);
+	let pts = [];
+  pts.push({x:-1.4745501,y:-0.17393055});
+  pts.push({x:-1.272389,y:0.24901979});
+  pts.push({x:-0.86602535,y:0.49999997});
+  pts.push({x:-0.59496015,y:0.45621268});
+  pts.push({x:-0.27805348,y:0.83946573});
+  pts.push({x:-0.58911391,y:1.2529424});
+  pts.push({x:-0.341367,y:1.5070869});
+  pts.push({x:-0.2441404,y:1.264922});
+  pts.push({x:0.0,y:1.0});
+  pts.push({x:-0.10735893,y:0.65610715});
+  pts.push({x:-0.26846659,y:0.45082396});
+  pts.push({x:0.07550248,y:0.3633411});
+  pts.push({x:0.27805348,y:0.83946565});
+  pts.push({x:0.76841368,y:0.75664295});
+  pts.push({x:0.8660254,y:0.5});
+  pts.push({x:0.66668334,y:0.35574187});
+  pts.push({x:0.58185055,y:0.46402799});
+  pts.push({x:0.46206079,y:0.46639019});
+  pts.push({x:0.40604191,y:0.185835});
+  pts.push({x:0.45819943,y:0.06843666});
+  pts.push({x:0.8660254,y:0.21535827});
+  pts.push({x:1.1944749,y:0.0});
+  pts.push({x:1.2167289,y:-0.17394378});
+  pts.push({x:0.86602537,y:-0.5});
+  pts.push({x:0.40830055,y:-0.35930998});
+  pts.push({x:0.2687878,y:-0.4655544});
+  pts.push({x:0.24650682,y:-0.85767912});
+  pts.push({x:0.57765764,y:-1.137406});
+  pts.push({x:0.50206647,y:-1.2412749});
+  pts.push({x:0.23108912,y:-1.3330387});
+  pts.push({x:0.17323996,y:-1.2281166});
+  pts.push({x:0.2246021,y:-1.1005062});
+  pts.push({x:0.0,y:-1.0});
+  pts.push({x:-0.01417337,y:-0.52258864});
+  pts.push({x:-0.27937859,y:-0.13603683});
+  pts.push({x:-0.21796445,y:-0.500582});
+  pts.push({x:-0.34168892,y:-0.80272582});
+  pts.push({x:-0.48895846,y:-0.89659803});
+  pts.push({x:-0.81309688,y:-0.7747346});
+  pts.push({x:-0.96594879,y:-0.81892015});
+  pts.push({x:-1.1780978,y:-0.68017503});
+  pts.push({x:-0.86602537,y:-0.49999997});
+  pts.push({x:-0.55395304,y:-0.68017511});
+  pts.push({x:-0.53987072,y:-0.42707612});
+  pts.push({x:-0.65456254,y:-0.31679528});
+  pts.push({x:-0.71109482,y:0.02484854});
+  pts.push({x:-0.86602532,y:0.10545164});
+  pts.push({x:-1.1895518,y:0.06152828});
+  pts.push({x:-1.4745501,y:-0.17393055});
+
+	let props0 = {scale:1,rot:0,points:pts};
+	let props1 = {scale:1,rot:Math.PI*4/3,points:pts};
+	let props2 = {scale:1,rot:Math.PI*2/3,points:pts};
+
+  let sqrt3 = Math.sqrt(3);
+  let deltaX = sqrt3*Math.cos(Math.PI/3);
+
+	templateRels[0].push({dx:sqrt3,dy:0,t:1,shape:5,props:props1,ref:null});
+	templateRels[0].push({dx:deltaX,dy:-1.5,t:2,shape:5,props:props2,ref:null});
+	templateRels[0].push({dx:-deltaX,dy:-1.5,t:1,shape:5,props:props1,ref:null});
+	templateRels[0].push({dx:-sqrt3,dy:0,t:2,shape:5,props:props2,ref:null});
+	templateRels[0].push({dx:-deltaX,dy:1.5,t:1,shape:5,props:props1,ref:null});
+	templateRels[0].push({dx:deltaX,dy:1.5,t:2,shape:5,props:props2,ref:null});
+
+	templateRels[1].push({dx:sqrt3,dy:0,t:2,shape:5,props:props2,ref:null});
+	templateRels[1].push({dx:deltaX,dy:-1.5,t:0,shape:5,props:props0,ref:null});
+	templateRels[1].push({dx:-deltaX,dy:-1.5,t:2,shape:5,props:props2,ref:null});
+	templateRels[1].push({dx:-sqrt3,dy:0,t:0,shape:5,props:props0,ref:null});
+	templateRels[1].push({dx:-deltaX,dy:1.5,t:2,shape:5,props:props2,ref:null});
+	templateRels[1].push({dx:deltaX,dy:1.5,t:0,shape:5,props:props0,ref:null});
+
+	templateRels[2].push({dx:sqrt3,dy:0,t:0,shape:5,props:props0,ref:null});
+	templateRels[2].push({dx:deltaX,dy:-1.5,t:1,shape:5,props:props1,ref:null});
+	templateRels[2].push({dx:-deltaX,dy:-1.5,t:0,shape:5,props:props0,ref:null});
+	templateRels[2].push({dx:-sqrt3,dy:0,t:1,shape:5,props:props1,ref:null});
+	templateRels[2].push({dx:-deltaX,dy:1.5,t:0,shape:5,props:props0,ref:null});
+	templateRels[2].push({dx:deltaX,dy:1.5,t:1,shape:5,props:props1,ref:null});
+
+	templateTarnsform(0.75337, 0);
+}
+
 // test for storing templates in JSON format
 function JSONShape() {
 	let str = `
